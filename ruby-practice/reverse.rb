4cd1e02e6c89dca96188f88085c9f7840a9f8814
LifeH <hwie.zhu@gmail.com>
2018-11-30 23:58:04 -0500

    completed reverse string challenge

--- ruby-practice/reverse.rb
+++ ruby-practice/reverse.rb
@@ -1,15 +1,23 @@
 def reverse(string)
     reverse_string = []
+    #capture the reverse_string in array
     i=0
     while i< string.length
         reverse_string.unshift(string[i])
+        #adds each character in string[i] to reverse_string
         i+=1
+        #increase index so it doesn't infinite loop
     end
     abc=reverse_string.join("")
+    #convert it back into string without any array or comma
     return abc
+    #call the variable
 end
+
 
 #Testing for the match to see if it works
 
 puts('reverse("abc") == "cba": ' + (reverse("abc") == "cba").to_s)
 #prints out "abc"=="cba" : true
+puts('reverse("a") == "a": ' + (reverse("a") == "a").to_s)
+#prints out "a" == "a" : true
