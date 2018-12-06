647d1fe2a6dd76b888306df3c9ee81d2b2cb3f40
LifeH <hwie.zhu@gmail.com>
2018-12-06 11:35:38 -0500

    fixed error, finished problem

--- ruby-practice/num_sum.rb
+++ ruby-practice/num_sum.rb
@@ -1,6 +1,7 @@
 #Find sum of integers 1 to max, and return that sum
 def sum_nums(max)
-	i = 0
+  i = 0
+  sum=0
   while i <= max
   sum = sum +i
   i+=1
@@ -8,5 +9,6 @@ def sum_nums(max)
 return sum
 end
 
+#sum has to be set equal to zero before it can be accessed outside of scope
 puts sum_nums(4) # => 10, because 1 + 2 + 3 + 4 = 10
 puts sum_nums(5) # => 15
