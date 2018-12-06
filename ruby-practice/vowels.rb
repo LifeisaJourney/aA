f84bcb4cdf3f6a584e1545173d3c9c817b8ad237
LifeH <hwie.zhu@gmail.com>
2018-12-05 23:51:57 -0500

    used while loop, then if condition to find the matches

--- ruby-practice/vowels.rb
+++ ruby-practice/vowels.rb
@@ -1,8 +1,19 @@
 #Take word as a string and return the amount of times the vowels: a, e, i , o, u appears within the word.
 
 def count_vowels(word)
-
-end
+    vowel= ["a","e","i","o","u"]
+      i = 0
+        counter = 0
+        while i < word.length
+  
+        if (word[i] == "a") || (word[i] == "e") || (word[i] == "i") || (word[i] == "o") || (word[i] == "u")
+  
+        counter +=1
+        end
+        i+=1
+      end
+        return counter
+  end
   
   puts count_vowels("bootcamp")  # => 3
   puts count_vowels("apple")     # => 2
