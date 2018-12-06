9e9f79fb1b2143029e748d8068121fdd4315215d
LifeH <hwie.zhu@gmail.com>
2018-12-05 23:51:16 -0500

    added vowels challenge

---
+++ ruby-practice/vowels.rb
@@ -0,0 +1,10 @@
+#Take word as a string and return the amount of times the vowels: a, e, i , o, u appears within the word.
+
+def count_vowels(word)
+
+end
+  
+  puts count_vowels("bootcamp")  # => 3
+  puts count_vowels("apple")     # => 2
+  puts count_vowels("pizza")     # => 2
+  
