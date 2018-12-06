15b9b5cb6eaab8e5fac8447a343dcdd6e2b91dd6
LifeH <hwie.zhu@gmail.com>
2018-12-05 23:47:47 -0500

    completed challenge

--- ruby-practice/countE.rb
+++ ruby-practice/countE.rb
@@ -1,6 +1,14 @@
 #Take word as a string and return the number of e from the string
 def count_e(word)
-
+	i = 0
+  counter = 0
+  while i < word.length
+    if word[i] == "e"
+       counter +=1
+    end
+    i+=1
+  end
+   return counter
 end
 
 puts count_e("movie") # => 1
