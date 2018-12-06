4bbb3d20e25d424b4ef26015f9baf74898fc09d8
LifeH <hwie.zhu@gmail.com>
2018-12-05 23:57:10 -0500

    problem in console

--- ruby-practice/num_sum.rb
+++ ruby-practice/num_sum.rb
@@ -1,6 +1,11 @@
 #Find sum of integers 1 to max, and return that sum
 def sum_nums(max)
-
+	i = 0
+  while i <= max
+  sum = sum +i
+  i+=1
+  end
+return sum
 end
 
 puts sum_nums(4) # => 10, because 1 + 2 + 3 + 4 = 10
