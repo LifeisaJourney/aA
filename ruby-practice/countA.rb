14b420fffadcb83f028cfaedd8812f47d3b6c610
LifeH <hwie.zhu@gmail.com>
2018-12-05 23:49:31 -0500

    added countA instruction

---
+++ ruby-practice/countA.rb
@@ -0,0 +1,10 @@
+#Take word as a string and return the amount of times (a and A) appears as an integer 
+
+def count_a(word)
+
+end
+
+puts count_a("application")  # => 2
+puts count_a("bike")         # => 0
+puts count_a("Arthur")       # => 1
+puts count_a("Aardvark")     # => 3
