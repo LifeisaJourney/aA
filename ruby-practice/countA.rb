7e273eb17a3f84651fc97fd729e6cc387016ca65
LifeH <hwie.zhu@gmail.com>
2018-12-05 23:50:01 -0500

    finished problem

--- ruby-practice/countA.rb
+++ ruby-practice/countA.rb
@@ -1,7 +1,15 @@
 #Take word as a string and return the amount of times (a and A) appears as an integer 
 
 def count_a(word)
-
+	i = 0 
+  	counter = 0 
+  while i < word.length
+    if (word[i] == "a") || (word[i] == "A")
+      counter +=1
+    end
+    i+=1
+  end
+  return counter
 end
 
 puts count_a("application")  # => 2
