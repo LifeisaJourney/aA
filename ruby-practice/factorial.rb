36922ee914be95e118e84fa7f5254c232adbfc2a
LifeH <hwie.zhu@gmail.com>
2018-12-06 13:29:01 -0500

    finished problem

--- ruby-practice/factorial.rb
+++ ruby-practice/factorial.rb
@@ -1,5 +1,12 @@
+#finding the multiplication factors from num
 def factorial(num)
-
+	i = 1
+  multi=1
+  while i <= num
+  multi*=i
+  i+=1 
+  end
+  return multi
 end
 
 puts factorial(3) # => 6, because 1 * 2 * 3 = 6
