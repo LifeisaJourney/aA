3633df3eaa6a185af8443c0d96e0c5311615d303
LifeH <hwie.zhu@gmail.com>
2018-12-08 17:27:13 -0500

    reverse.rb problem

--- ruby-practice/reverse.rb
+++ ruby-practice/reverse.rb
@@ -1,27 +1,9 @@
-def reverse(string)
-    reverse_string = []
-    #capture the reverse_string in array
-    i=0
-    while i< string.length
-        reverse_string.unshift(string[i])
-        #adds each character in string[i] to reverse_string
-        i+=1
-        #increase index so it doesn't infinite loop
-    end
-    abc=reverse_string.join("")
-    #convert it back into string without any array or comma
-    return abc
-    #call the variable
+#reverse the string input as word and output it in reverse. 
+def reverse(word)
+
+ 
 end
 
-
-#Testing for the match to see if it works
-
-puts('reverse("abc") == "cba": ' + (reverse("abc") == "cba").to_s)
-#prints out "abc"=="cba" : true
-puts('reverse("a") == "a": ' + (reverse("a") == "a").to_s)
-#prints out "a" == "a" : true
-puts('reverse("I am a happy pet") == "tep yppah a ma I": ' + (reverse("I am a happy pet") == "tep yppah a ma I").to_s)
-#prints out "I am a happy pet" == "tep yppah a ma I" : true
-puts('reverse("") == "": ' + (reverse("") == "").to_s)
-#prints out "" == "" : true
+puts reverse("cat")          # => "tac"
+puts reverse("programming")  # => "gnimmargorp"
+puts reverse("bootcamp")     # => "pmactoob"
