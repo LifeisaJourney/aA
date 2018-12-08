1bfbee66382ff4ab95b3824d7d28cfffb2f911ce
LifeH <hwie.zhu@gmail.com>
2018-12-08 17:33:54 -0500

    added comments and solved the problem

--- ruby-practice/reverse.rb
+++ ruby-practice/reverse.rb
@@ -1,8 +1,18 @@
 #reverse the string input as word and output it in reverse. 
-
+# strategy: work out the problem from a desending iteration. 
+# return the word based on while loop
+# store the returned values back into an empty string
 def reverse(word)
+    i = word.length - 1
+    reversed = ""
+    while i >= 0
+        char = word[i]
+        reversed = char + reversed
+        i -= 1
+    end
+    return reversed
 end
-
+#view the result by ruby reverse.rb
 puts reverse("cat")          # => "tac"
 puts reverse("programming")  # => "gnimmargorp"
 puts reverse("bootcamp")     # => "pmactoob"
