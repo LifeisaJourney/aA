d5ede7ab47c7a91e11e7093ed250f5fe3375456c
LifeH <hwie.zhu@gmail.com>
2018-12-09 16:59:29 -0500

    the order of the condition and while loop matter

--- ruby-practice/palindrome.rb
+++ ruby-practice/palindrome.rb
@@ -1,6 +1,17 @@
 #takes a word as a string and returns the operator checking if the word is the same as it is in reverse. Returns boolean. 
 def is_palindrome(word)
-
+    i=word.length-1
+    palin_string=""
+    while i >= 0 
+        palin_string = palin_string + word[i]
+        i-=1
+    end
+    # puts palin_string
+    if word == palin_string
+        return true
+    else
+        return false
+    end
 end
 
 puts is_palindrome("racecar")  # => true
