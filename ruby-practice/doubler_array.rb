2b84b0db7f56b256ea82fe7d1061a3cdd9477960
LifeH <hwie.zhu@gmail.com>
2018-12-10 18:05:51 -0500

    challenge completed

--- ruby-practice/doubler_array.rb
+++ ruby-practice/doubler_array.rb
@@ -1,5 +1,15 @@
+# Takes the array indicies as element and return it as a double in value
+#returns as a new array
 def doubler(numbers)
-
+    doubled= []
+    i=0
+    while i < numbers.length
+        old_container= numbers[i]
+        new_container=old_container * 2
+        doubled << new_container
+        i+=1
+    end
+    return doubled
 end
 
 doubler([7,1,8]) #[14,2,16]
