c72eee0e7e402eb6bba63287d4e6b1f9777897ea
LifeH <hwie.zhu@gmail.com>
2018-12-11 18:58:48 -0500

    added initial commit on the problems

---
+++ ruby-practice/element_times_index.rb
@@ -0,0 +1,7 @@
+def element_times_index(numbers)
+    
+end
+
+print element_times_index([4, 7, 6, 5]) # => [0, 7, 12, 15]
+puts
+print element_times_index([1, 1, 1, 1, 1, 1]) #=> [0, 1, 2, 3, 4, 5]
