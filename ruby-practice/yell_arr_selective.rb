31b38532fffd45d9033696a021f40be36228e5ef
LifeH <hwie.zhu@gmail.com>
2018-12-11 18:41:58 -0500

    finished problem

--- ruby-practice/yell_arr_selective.rb
+++ ruby-practice/yell_arr_selective.rb
@@ -1,7 +1,14 @@
 #taking strings within the array and adding an exclaimation to the end of the string
 
 def yell(words)
-    
+    i=0
+    yelling = []
+    while i < words.length
+        yelling << words[i] + "!"
+
+        i+=1
+    end
+    return yelling
 end
 
 print yell(["hello", "world"])
