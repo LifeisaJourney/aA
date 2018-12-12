2e8461155ca20be1b78c3435c15ffcc272ffe75e
LifeH <hwie.zhu@gmail.com>
2018-12-11 23:23:13 -0500

    solved the problem

--- ruby-practice/element_times_index.rb
+++ ruby-practice/element_times_index.rb
@@ -1,5 +1,15 @@
+# Takes an array of numbers and returns a new array containing new array multiplied by its own index
+
 def element_times_index(numbers)
-    
+    i=0
+    new_array=[]
+    while i < numbers.length
+        position= i 
+        multiplier= numbers[i] * i 
+        new_array << multiplier
+        i+=1
+    end
+    return new_array
 end
 
 print element_times_index([4, 7, 6, 5]) # => [0, 7, 12, 15]
