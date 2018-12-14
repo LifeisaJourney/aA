d7a9595c7b5bea15ceb26fae48cbda9e0994f070
LifeH <hwie.zhu@gmail.com>
2018-12-13 22:07:54 -0500

    used unshift to add the values to the front of the array

--- ruby-practice/reverse_range.rb
+++ ruby-practice/reverse_range.rb
@@ -1,9 +1,17 @@
 # accepts array
 # min and max not included in the array
-# return the array in reverse
+# return the array in reverse by unshift method
 
 def reverse_range(min, max)
-
+    i=min 
+    reversed_arr =[]
+    while i < max 
+        if i < max && i != min
+            reversed_arr.unshift(i) 
+        end
+      i+=1
+    end
+    reversed_arr
 end
 
 print reverse_range(10, 17) # => [16, 15, 14, 13, 12, 11]
