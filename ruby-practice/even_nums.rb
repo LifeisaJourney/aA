51c368f8d54300089c36c553851229b07d5f7d27
LifeH <hwie.zhu@gmail.com>
2018-12-13 19:08:45 -0500

    solved the problem

--- ruby-practice/even_nums.rb
+++ ruby-practice/even_nums.rb
@@ -1,6 +1,15 @@
 #take an array of numbers and return the even integers
+#put the if condition within the while loop
 def even_nums(max)
-
+    i = 0
+    even_digits=[]
+    while i <=max
+        if i %2 ==0
+        even_digits<<i 
+        end
+        i+=1
+    end
+    return even_digits
 end
 
 print even_nums(10) # => [0, 2, 4, 6, 8, 10]
