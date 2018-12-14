bdcd317e9d4dafeda340d2b277831b1cdcf3711d
LifeH <hwie.zhu@gmail.com>
2018-12-13 21:43:49 -0500

    answer finished, the if condition

--- ruby-practice/odd_range.rb
+++ ruby-practice/odd_range.rb
@@ -1,6 +1,14 @@
 #Takes the array and return the array of values for between min and max and odd 
 def odd_range(min, max)
-    
+    i=0
+    odd_only = []
+    while i <= max 
+        if i % 2 == 1 && i >= min
+            odd_only << i 
+        end
+        i+=1 
+    end
+    return odd_only
 end
 
 print odd_range(11, 18) # => [11, 13, 15, 17]
