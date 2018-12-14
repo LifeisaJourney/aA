7414b9d753972d608d313d5f2ef743d4ad98330b
LifeH <hwie.zhu@gmail.com>
2018-12-13 22:15:59 -0500

    added alternative solution

--- ruby-practice/reverse_range.rb
+++ ruby-practice/reverse_range.rb
@@ -4,6 +4,11 @@
 
 
 #alternative solution is to countdown with decreasing iteration
+# i = max - 1
+# while i < max 
+# reversed_arr << i 
+# i -=1 
+# end 
 def reverse_range(min, max)
     i=min 
     reversed_arr =[]
@@ -19,3 +24,4 @@ end
 print reverse_range(10, 17) # => [16, 15, 14, 13, 12, 11]
 puts
 print reverse_range(1, 7)   # => [6, 5, 4, 3, 2]
+
