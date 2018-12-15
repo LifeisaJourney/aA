516e7568867dedd2cd802730d8480a4ba8d16946
LifeH <hwie.zhu@gmail.com>
2018-12-15 16:42:12 -0500

    initial commit

---
+++ ruby-practice/factors_of.rb
@@ -0,0 +1,13 @@
+def factors_of(num)
+    
+end
+
+print factors_of(3) #=> [1, 3]
+puts
+print factors_of(4) # => [1,2,4]
+puts
+print factors_of(8) #=> [1, 2,4,8]
+puts
+print factors_of(9) # => [1,3, 9]
+puts
+print factors_of(16) #=> [1, 2,4,8,16]
