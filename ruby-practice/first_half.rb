5a1aea0df6c449f610394f7459e7c9daa89fd724
LifeH <hwie.zhu@gmail.com>
2018-12-15 11:23:12 -0500

    resolved the problem using condition in while loop with float

--- ruby-practice/first_half.rb
+++ ruby-practice/first_half.rb
@@ -2,7 +2,14 @@
 #first half of the element is returned in the new array
 # if it is odd numbered of elements, return the middle value too
 def first_half(array)
+    new_array = []
+    i = 0
+    while i < array.length / 2.0
+        new_array << array[i]
 
+        i+=1
+    end
+    return new_array
 end
 
 print first_half(["Brian", "Abby", "David", "Ommi"]) # => ["Brian", "Abby"]
