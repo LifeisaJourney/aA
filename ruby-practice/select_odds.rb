85f740b312cd4c119d349adcd641725fa6a4689f
LifeH <hwie.zhu@gmail.com>
2018-12-16 17:25:30 -0500

    added pseudocode and completed

--- ruby-practice/select_odds.rb
+++ ruby-practice/select_odds.rb
@@ -1,5 +1,18 @@
+#take in array of numbers
+#return new array with only odd numbers
+
 def select_odds(numbers)
-    
+	new_array = []
+  	i=0
+  	while i < numbers.length
+      if numbers[i] % 2 ==1
+        new_array << numbers[i]
+      end
+      
+      i+=1
+    end
+  
+  return new_array
 end
 
 print select_odds([13, 4, 3, 7, 6, 11]) # => [13, 3, 7, 11]
