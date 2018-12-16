0f4a713eeb0199a556d86d30c5af3f77b58006d2
LifeH <hwie.zhu@gmail.com>
2018-12-16 17:26:09 -0500

    initial commit

---
+++ ruby-practice/select_long_words.rb
@@ -0,0 +1,7 @@
+def select_long_words(words)
+
+end
+
+print select_long_words(["what", "are", "we", "eating", "for", "dinner"]) # => ["eating", "dinner"]
+puts
+print select_long_words(["keep", "coding"])                               # => ["coding"]
