edcb023be42e6dc51334649392b5d300c8b97e72
LifeH <hwie.zhu@gmail.com>
2018-12-16 17:27:50 -0500

    initial commit

---
+++ ruby-practice/sum_elements.rb
@@ -0,0 +1,7 @@
+def sum_elements(arr1, arr2)
+
+end
+
+print sum_elements([7, 4, 4], [3, 2, 11])                            # => [10, 6, 15]
+puts
+print sum_elements(["cat", "pizza", "boot"], ["dog", "pie", "camp"]) # => ["catdog", "pizzapie", "bootcamp"]
