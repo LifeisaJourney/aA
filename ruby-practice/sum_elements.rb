a588c4f8906fa41067b3a0a32c2abc4cc87cd80d
LifeH <hwie.zhu@gmail.com>
2018-12-16 17:28:05 -0500

    added pseudocode, finished assignment

--- ruby-practice/sum_elements.rb
+++ ruby-practice/sum_elements.rb
@@ -1,5 +1,14 @@
+# Takes 2 arrays as arguments in the parameter
+#Return new array with the elements in the arrays concatenated. 
 def sum_elements(arr1, arr2)
-
+	i=0
+  	new_array = []
+  	while i < arr1.length
+      sum_array= arr1[i] + arr2[i]
+      new_array << sum_array
+      i+=1
+    end 
+  return new_array  
 end
 
 print sum_elements([7, 4, 4], [3, 2, 11])                            # => [10, 6, 15]
