1f255ac56a6fcdf7756642ec22bc77c25ae0f508
LifeH <hwie.zhu@gmail.com>
2018-12-16 17:26:52 -0500

    added pseudocode, finished assignment

--- ruby-practice/select_long_words.rb
+++ ruby-practice/select_long_words.rb
@@ -1,5 +1,16 @@
+#take an array of words
+#return new array with words from old array
+#select for characters longer than 4 characters
 def select_long_words(words)
-
+	i = 0
+  	long_char = []
+  	while i < words.length
+  		if words[i].length > 4 
+          long_char << words[i]
+        end
+    	i+=1
+    end
+   	return long_char  
 end
 
 print select_long_words(["what", "are", "we", "eating", "for", "dinner"]) # => ["eating", "dinner"]
