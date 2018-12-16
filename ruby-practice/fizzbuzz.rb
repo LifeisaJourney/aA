0bb221d9946a567834faa79b215de3fc67cf8797
LifeH <hwie.zhu@gmail.com>
2018-12-16 17:30:21 -0500

    completed assignment

--- ruby-practice/fizzbuzz.rb
+++ ruby-practice/fizzbuzz.rb
@@ -3,7 +3,15 @@
 # i divides by 4 OR 6
 # DOES NOT i divide by 4 AND 6
 def fizz_buzz(max)
-	
+	new_array=[]
+  	i=1
+  	while i < max
+     if (i % 4 == 0 ||  i % 6 == 0)  && !(i % 4 ==0 && i % 6 == 0)
+       new_array << i
+     end
+      i+=1
+    end
+  	return new_array	
 end
 
 print fizz_buzz(20) # => [4, 6, 8, 16, 18]
