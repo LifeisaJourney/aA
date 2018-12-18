d65d2dda4029516d2d57b2ec26c4f0ca4996bdfb
LifeH <hwie.zhu@gmail.com>
2018-12-18 15:46:01 -0500

    completed solving using enumerable method

--- ruby-practice/introduction_practice/first_in_array.rb
+++ ruby-practice/introduction_practice/first_in_array.rb
@@ -1,7 +1,13 @@
 # takes array and two elements
 # return element that appears earlier in the array.
 def first_in_array(arr, el1, el2)
-
+    ar2 = arr.index(el2)
+    ar1 = arr.index(el1)
+    if ar1<ar2
+        return el1
+    else 
+        return el2
+    end
 end
 
 puts first_in_array(["a", "b", "c", "d"], "d", "b"); #=> "b"
