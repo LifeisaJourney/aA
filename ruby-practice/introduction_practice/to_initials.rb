b02513ec7dc5172ea59e7023e3c58a47ec7099cf
LifeH <hwie.zhu@gmail.com>
2018-12-17 23:14:06 -0500

    hacky solution

--- ruby-practice/introduction_practice/to_initials.rb
+++ ruby-practice/introduction_practice/to_initials.rb
@@ -1,5 +1,9 @@
 def to_initials(name)
-
+    str = ""
+    partial = name.split(" ")
+    first_letter = partial[0][0]
+    second_letter = partial[1][0]
+    str << first_letter + second_letter
 end
 
 puts to_initials("Kelvin Bridges") #=> "KB"
