cb64acd2e5cff8bf0b7d0ef66a0b51f43cb3c4ed
LifeH <hwie.zhu@gmail.com>
2018-12-18 16:03:43 -0500

    initial commit

---
+++ ruby-practice/introduction_practice/abbreviate_sentence.rb
@@ -0,0 +1,6 @@
+def abbreviate_sentence(sent)
+	
+end
+
+puts abbreviate_sentence("follow the yellow brick road") # => "fllw the yllw brck road"
+puts abbreviate_sentence("what a wonderful life")        # => "what a wndrfl life"
