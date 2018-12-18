98ee48cae53af08a5370b90ba1a73a944fbd5be4
LifeH <hwie.zhu@gmail.com>
2018-12-18 10:31:25 -0500

    completed

--- ruby-practice/introduction_practice/to_initials.rb
+++ ruby-practice/introduction_practice/to_initials.rb
@@ -1,11 +1,18 @@
 # Take the inital letters of each word and return in string.
 # Find alternative solution to it
+# Used enumerates to solve it
+
 def to_initials(name)
+    # str = ""
+    # partial = name.split(" ")
+    # first_letter = partial[0][0]
+    # second_letter = partial[1][0]
+    # str << first_letter + second_letter
+    
     str = ""
     partial = name.split(" ")
-    first_letter = partial[0][0]
-    second_letter = partial[1][0]
-    str << first_letter + second_letter
+    partial.each {|part| str=str+part[0]}
+    return str
 end
 
 puts to_initials("Kelvin Bridges") #=> "KB"
