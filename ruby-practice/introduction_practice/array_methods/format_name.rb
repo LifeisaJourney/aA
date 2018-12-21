ce26089722b8ac85c34bbddf837ddedc4953f6f3
LifeH <hwie.zhu@gmail.com>
2018-12-21 10:06:16 -0500

    completed problem

--- ruby-practice/introduction_practice/array_methods/format_name.rb
+++ ruby-practice/introduction_practice/array_methods/format_name.rb
@@ -1,8 +1,20 @@
 # Hint: use str.upcase and str.downcase
 # "abc".upcase # => "ABC"
+#take string , return string
+# take string and turn array
+# use array to capitalize method the words
+
 
 def format_name(str)
-
+    empty_array = []
+    empty_string = "" 
+    array_turner= str.split(" ")
+    array_turner.each do |word|
+        caps = word.capitalize
+        empty_array << caps
+    end
+    empty_string = empty_array.join(" ")
+    return empty_string
 end
   
   puts format_name("chase WILSON") # => "Chase Wilson"
