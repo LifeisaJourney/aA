b6d23d4ed9f2ed471b5498c80525c40d71422f9b
LifeH <hwie.zhu@gmail.com>
2018-12-21 10:02:32 -0500

    initial commit

---
+++ ruby-practice/introduction_practice/array_methods/format_name.rb
@@ -0,0 +1,10 @@
+# Hint: use str.upcase and str.downcase
+# "abc".upcase # => "ABC"
+
+def format_name(str)
+
+end
+  
+  puts format_name("chase WILSON") # => "Chase Wilson"
+  puts format_name("brian CrAwFoRd scoTT") # => "Brian Crawford Scott"
+  
