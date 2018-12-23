04945d58653675d5f373fe517f47a4511c48dbfd
LifeH <hwie.zhu@gmail.com>
2018-12-22 21:47:53 -0500

    solved the challenge

--- ruby-practice/introduction_practice/array_methods/valid_name.rb
+++ ruby-practice/introduction_practice/array_methods/valid_name.rb
@@ -6,7 +6,25 @@
 # "a".upcase # => "A"
 
 def is_valid_name(str)
+    parts = str.split(" ")
+    if parts.length < 2
+        return false
+    end
 
+    parts.each do |part|
+       if !is_cap(part) 
+            return false
+       end        
+    end
+    return true
+end
+
+def is_cap(word)
+        if word[0] == word[0].upcase && word[1..-1] == word[1..-1].downcase 
+            return true
+        else
+            return false
+        end
 end
 
 puts is_valid_name("Kush Patel")       # => true
