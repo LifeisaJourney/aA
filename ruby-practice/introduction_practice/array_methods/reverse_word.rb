fbcced52ceeb5f73893284b4c987669294bd759f
LifeH <hwie.zhu@gmail.com>
2018-12-26 12:54:57 -0500

    added solution, but suspect it is still array form not string

--- ruby-practice/introduction_practice/array_methods/reverse_word.rb
+++ ruby-practice/introduction_practice/array_methods/reverse_word.rb
@@ -3,7 +3,14 @@
 # Do not word not order
 # Return in string
 def reverse_words(sent)
-
+    empty_string = ""
+    arr= sent.split(" ")
+    arr.each do |part|
+    new_string= part.reverse
+    #new_string = part.join(" ")
+     empty_string = empty_string + " " + new_string
+  end
+  	return empty_string
 end
   
   puts reverse_words('keep coding') # => 'peek gnidoc'
