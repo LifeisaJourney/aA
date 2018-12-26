667e5b99988ec7fe4a079f53c6b0ae0ce51bfa84
LifeH <hwie.zhu@gmail.com>
2018-12-26 13:03:59 -0500

    returned in string format

--- ruby-practice/introduction_practice/array_methods/reverse_word.rb
+++ ruby-practice/introduction_practice/array_methods/reverse_word.rb
@@ -2,17 +2,27 @@
 # Returns order of characters in each word reversed: each?
 # Do not word not order
 # Return in string
-def reverse_words(sent)
-    empty_string = ""
-    arr= sent.split(" ")
-    arr.each do |part|
-    new_string= part.reverse
-    #new_string = part.join(" ")
-     empty_string = empty_string + " " + new_string
-  end
-  	return empty_string
-end
+# def reverse_words(sent)
+#     empty_string = ""
+#     arr= sent.split(" ")
+#     arr.each do |part|
+#     new_string= part.reverse
+#     #new_string = part.join(" ")
+#      empty_string = empty_string + " " + new_string
+#   end
+#   	return empty_string
+# end
   
+
+def reverse_words(sent)
+    new_words = []
+    words_in_array = sent.split(" ") #turns to array
+    words_in_array.each { |word| new_words << word.reverse}
+    new_sent = new_words.join(" ")
+    return new_sent
+
+end
+
   puts reverse_words('keep coding') # => 'peek gnidoc'
   puts reverse_words('simplicity is prerequisite for reliability') # => 'yticilpmis si etisiuqererp rof ytilibailer'
   
