aa63c46e7548d35085e7c0afb161d66874b7ff6a
LifeH <hwie.zhu@gmail.com>
2018-12-26 00:10:54 -0500

    solved problem

--- ruby-practice/introduction_practice/array_methods/array_translate.rb
+++ ruby-practice/introduction_practice/array_methods/array_translate.rb
@@ -6,7 +6,10 @@ def array_translate(array)
     new_string = ""
     i=0
     while i < array.length
-
+        ele1 = array[0]
+        ele2 = array[1]
+        ele2.times {new_string+= ele1}
+        i+=2
     end
     return new_string
 end
