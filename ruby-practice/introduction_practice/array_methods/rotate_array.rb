e55603479f968c1a30b0cd3206777f86d64ee0b0
LifeH <hwie.zhu@gmail.com>
2018-12-26 13:05:00 -0500

    initial commit

---
+++ ruby-practice/introduction_practice/array_methods/rotate_array.rb
@@ -0,0 +1,9 @@
+def rotate_array(arr, num)
+
+end
+
+print rotate_array([ "Matt", "Danny", "Mashu", "Matthias" ], 1) # => [ "Matthias", "Matt", "Danny", "Mashu" ]
+puts
+
+print rotate_array([ "a", "b", "c", "d" ], 2) # => [ "c", "d", "a", "b" ]
+puts
