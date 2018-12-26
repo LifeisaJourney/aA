56cb066cde262e6e5d10200b8a98b4ede7d314bd
LifeH <hwie.zhu@gmail.com>
2018-12-26 18:32:09 -0500

    initial commit

---
+++ ruby-practice/introduction_practice/array_methods/combinations.rb
@@ -0,0 +1,9 @@
+def combinations(arr)
+
+end
+
+print combinations(["a", "b", "c"]); # => [ [ "a", "b" ], [ "a", "c" ], [ "b", "c" ] ]
+puts
+
+print combinations([0, 1, 2, 3]); # => [ [ 0, 1 ], [ 0, 2 ], [ 0, 3 ], [ 1, 2 ], [ 1, 3 ], [ 2, 3 ] ]
+puts
