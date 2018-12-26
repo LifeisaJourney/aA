479221e7dad5078ca69f864e205290475af55aac
LifeH <hwie.zhu@gmail.com>
2018-12-26 15:50:27 -0500

    solved the problem

--- ruby-practice/introduction_practice/array_methods/rotate_array.rb
+++ ruby-practice/introduction_practice/array_methods/rotate_array.rb
@@ -1,7 +1,12 @@
 # use pop and unshift
 # use num as reference to times method 
 def rotate_array(arr, num)
-
+    new_array = []
+    num.times do 
+        ele = arr.pop
+        new_array = arr.unshift(ele)
+    end
+    return new_array
 end
 
 print rotate_array([ "Matt", "Danny", "Mashu", "Matthias" ], 1) # => [ "Matthias", "Matt", "Danny", "Mashu" ]
