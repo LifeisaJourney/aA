7f37c60b985b082aae8f27ab800e72f8a60bee0c
LifeH <hwie.zhu@gmail.com>
2018-12-26 22:49:41 -0500

    unique counts by removing double counting

--- ruby-practice/introduction_practice/array_methods/opposite_count.rb
+++ ruby-practice/introduction_practice/array_methods/opposite_count.rb
@@ -2,7 +2,16 @@
 #return the pair of opposite numbers 
 # return a counter
 def opposite_count(nums)
+    count = 0
 
+    nums.each_with_index do |ele1, idx1|
+        nums.each_with_index do |ele2, idx2|
+            if ele1 + ele2 ==0 && idx2 > idx1
+                count+=1    
+            end
+        end
+    end
+    return count
 end
 
 puts opposite_count([ 2, 5, 11, -5, -2, 7 ]) # => 2
