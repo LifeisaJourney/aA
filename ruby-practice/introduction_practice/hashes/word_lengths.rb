6cbe5ab4c2d1611df018af9244474cdb1975fec9
LifeH <hwie.zhu@gmail.com>
2018-12-29 16:47:13 -0500

    finished problem

--- ruby-practice/introduction_practice/hashes/word_lengths.rb
+++ ruby-practice/introduction_practice/hashes/word_lengths.rb
@@ -2,7 +2,13 @@
 # Declare empty hash
 # add word.length into empty hash
 def word_lengths(sentence)
+    new_hashes = {}
+    turn_array = sentence.split(" ")
 
+    turn_array.each do |word|
+        new_hashes[word] = word.length
+    end
+    return new_hashes
 end
 
 puts word_lengths("this is fun") #=> {"this"=>4, "is"=>2, "fun"=>3}
