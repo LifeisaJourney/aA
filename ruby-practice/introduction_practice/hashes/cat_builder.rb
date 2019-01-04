b7c3b94c0dd2d20b6032ce4241083ec8a8d78752
LifeH <hwie.zhu@gmail.com>
2019-01-04 13:53:37 -0500

    solved problem with initializing hash

--- ruby-practice/introduction_practice/hashes/cat_builder.rb
+++ ruby-practice/introduction_practice/hashes/cat_builder.rb
@@ -2,7 +2,8 @@
 #return hash containing those key values
 
 def cat_builder(name_str, color_str, age_num)
-
+    initialize_hash = { "name" => name_str, "color" => color_str, "age" => age_num}
+    return initialize_hash
 end
 
 print cat_builder("Whiskers", "orange", 3) #=> {"name"=>"Whiskers", "color"=>"orange", "age"=>3}
