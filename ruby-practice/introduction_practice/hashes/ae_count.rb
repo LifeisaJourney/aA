8adb9a069a951ede13e15042f23ce3c0439aa789
LifeH <hwie.zhu@gmail.com>
2019-01-04 18:40:05 -0500

    completed problem with counter

--- ruby-practice/introduction_practice/hashes/ae_count.rb
+++ ruby-practice/introduction_practice/hashes/ae_count.rb
@@ -1,5 +1,17 @@
-def ae_count(str)
+#takes string
+#returns hash
+#look for lowercase ae characters only
+#return value as counter of how many times ae appears
+#counter[char] means that counter with the char key will gain value of +1 
 
+def ae_count(str)
+    counter = {"a" => 0, "e" => 0}
+    str.each_char do |char|
+        if (char =="a" || char =="e")
+            counter[char] += 1 
+        end
+    end
+    return counter 
 end
 
 puts ae_count("everyone can program") #=> {"a"=>2, "e"=>3}
