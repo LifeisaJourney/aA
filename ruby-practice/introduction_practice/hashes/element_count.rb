3fdf981a40a2d6206f842658f578442c9f27290c
LifeH <hwie.zhu@gmail.com>
2019-01-04 23:43:34 -0500

    solved problem

--- ruby-practice/introduction_practice/hashes/element_count.rb
+++ ruby-practice/introduction_practice/hashes/element_count.rb
@@ -1,9 +1,14 @@
 # Takes array
 # Returns hash
 # Within hash contains count of each element in array without selection
-
+# since I want to count every element in the hash, I will use Hash.new(0)
+# can't use {"a" => 0, "b" => 0} with explicit elements.
 def element_count(arr)
-
+    count = Hash.new(0)
+    arr.each do |char|
+        count[char] +=1
+    end
+    return count
 end
 
 puts element_count(["a", "b", "a", "a", "b"]) #=> {"a"=>3, "b"=>2}
