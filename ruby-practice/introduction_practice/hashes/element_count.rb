a1043462357cbb5714eeb2f34b2f947ed267648b
LifeH <hwie.zhu@gmail.com>
2019-01-04 22:30:47 -0500

    initial commit

---
+++ ruby-practice/introduction_practice/hashes/element_count.rb
@@ -0,0 +1,6 @@
+def element_count(arr)
+
+end
+
+puts element_count(["a", "b", "a", "a", "b"]) #=> {"a"=>3, "b"=>2}
+puts element_count(["red", "red", "blue", "green"]) #=> {"red"=>2, "blue"=>1, "green"=>1}
