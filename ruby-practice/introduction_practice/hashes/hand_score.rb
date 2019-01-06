a94413d2059af753dcfd7949cae0419f8938251d
LifeH <hwie.zhu@gmail.com>
2019-01-06 09:17:25 -0500

    solved problem by using conditional if and adding to score

--- ruby-practice/introduction_practice/hashes/hand_score.rb
+++ ruby-practice/introduction_practice/hashes/hand_score.rb
@@ -2,7 +2,19 @@
 #the string can be upcase or downcase
 
 def hand_score(hand)
-
+    score = 0 
+    hand.each_char do |word|
+    
+    if (word == "a" || word == "A")
+        score += 4
+    elsif (word == "k" || word =="K")
+        score += 3
+    elsif (word == "q" || word == "Q")
+        score += 2 
+    elsif (word =="j" || word == "J")
+        score += 1
+    end
+    return score
 end
 
 puts hand_score("AQAJ") #=> 11
