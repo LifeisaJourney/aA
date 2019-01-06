df929998b6177ab0e73f520e56b624da17531edf
LifeH <hwie.zhu@gmail.com>
2019-01-06 09:19:43 -0500

    DRY coding alternative answer

--- ruby-practice/introduction_practice/hashes/hand_score.rb
+++ ruby-practice/introduction_practice/hashes/hand_score.rb
@@ -1,20 +1,28 @@
 #takes string and returns integer counter
 #the string can be upcase or downcase
 
-def hand_score(hand)
-    score = 0 
-    hand.each_char do |word|
+# def hand_score(hand)
+#     score = 0 
+#     hand.each_char do |word|
     
-    if (word == "a" || word == "A")
-        score += 4
-    elsif (word == "k" || word =="K")
-        score += 3
-    elsif (word == "q" || word == "Q")
-        score += 2 
-    elsif (word =="j" || word == "J")
-        score += 1
-    end
-    return score
+#     if (word == "a" || word == "A")
+#         score += 4
+#     elsif (word == "k" || word =="K")
+#         score += 3
+#     elsif (word == "q" || word == "Q")
+#         score += 2 
+#     elsif (word =="j" || word == "J")
+#         score += 1
+#     end
+#     return score
+# end
+
+# puts hand_score("AQAJ") #=> 11
+# puts hand_score("jJka") #=> 9
+
+
+def hand_score(hand)
+
 end
 
 puts hand_score("AQAJ") #=> 11
