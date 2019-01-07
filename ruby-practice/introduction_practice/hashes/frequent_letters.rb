506f71cf3d546a63a7a52cf2db8299611d7c4a65
LifeH <hwie.zhu@gmail.com>
2019-01-06 22:17:22 -0500

    initial commit

---
+++ ruby-practice/introduction_practice/hashes/frequent_letters.rb
@@ -0,0 +1,15 @@
+#count the number value for each char
+#print string and value
+#accept a string 
+#return in new array
+
+
+def frequent_letters(string)
+
+  end	
+  
+  print frequent_letters('mississippi') #=> ["i", "s"]
+  puts
+  print frequent_letters('bootcamp') #=> []
+  puts
+  
