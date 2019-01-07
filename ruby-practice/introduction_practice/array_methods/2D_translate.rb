9c0c7477b4d776439b502a6e9a88d2781cf90a7f
LifeH <hwie.zhu@gmail.com>
2019-01-06 22:24:26 -0500

    initial commit

---
+++ ruby-practice/introduction_practice/array_methods/2D_translate.rb
@@ -0,0 +1,20 @@
+def two_d_translate(arr)
+
+end
+
+arr_1 = [
+['boot', 3],
+['camp', 2],
+['program', 0]
+]
+
+print two_d_translate(arr_1) # => [ 'boot', 'boot', 'boot', 'camp', 'camp' ]
+puts
+
+arr_2 = [
+['red', 1],
+['blue', 4]
+]
+
+print two_d_translate(arr_2) # => [ 'red', 'blue', 'blue', 'blue', 'blue' ]
+puts
