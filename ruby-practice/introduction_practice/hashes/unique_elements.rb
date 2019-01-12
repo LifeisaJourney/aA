2da94007dbe781c802807dc1c7ca797cd520d8a2
LifeH <hwie.zhu@gmail.com>
2019-01-12 14:03:20 -0500

    returns the keys only

--- ruby-practice/introduction_practice/hashes/unique_elements.rb
+++ ruby-practice/introduction_practice/hashes/unique_elements.rb
@@ -4,16 +4,10 @@
 # remove duplicates
 
 def unique_elements(arr)
-    count = Hash.new(0)
-    new_array = []
-    arr.each do |char|
-        count[char]+=1    # gives the numerical value of char appearing. Will be used for determining how many times it appears
-        if count[char] > 1 
-            new_array << char
-        end
-    end
-
-    return new_array
+	hash = {}
+  	arr.each { |ele| hash[ele] = true}
+  	return hash.keys
+  
 end
 
 print unique_elements(['a', 'b', 'a', 'a', 'b', 'c']) #=> ["a", "b", "c"]
