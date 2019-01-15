53d0e8671db85075a45c0a191a99bbd654258a0f
LifeH <hwie.zhu@gmail.com>
2019-01-15 18:58:21 -0500

    added pseudocode and explaination for soln

--- ruby-practice/introduction_practice/adv_problems/map_by_name.rb
+++ ruby-practice/introduction_practice/adv_problems/map_by_name.rb
@@ -1,3 +1,7 @@
+#take array of hashes
+#return the string contained within the hash
+# choose for single element (hash) that contains the key of "name"
+# only arr.map will work since it will map for "name", if it was arr.each then it will also filter out for "type"
 def map_by_name(arr)
   return arr.map { |hash| hash["name"] }
 end
