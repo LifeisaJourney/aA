e58fd78399997e9bfd7b5ac3b205808b2f930694
LifeH <hwie.zhu@gmail.com>
2019-01-15 22:17:06 -0500

    solved problem

--- ruby-practice/introduction_practice/adv_problems/yell_sentence.rb
+++ ruby-practice/introduction_practice/adv_problems/yell_sentence.rb
@@ -1,9 +1,13 @@
 #takes string
 #returns string
 # add ! to the end of every word
-
+#split sentence to return each word
 def yell_sentence(sent)
-
+    words = sent.split(" ")
+    new_str = words.map do |word|
+        word.upcase + "!"
+    end
+    return new_str.join(" ")
 end
 
 puts yell_sentence("I have a bad feeling about this") #=> "I! HAVE! A! BAD! FEELING! ABOUT! THIS!"
