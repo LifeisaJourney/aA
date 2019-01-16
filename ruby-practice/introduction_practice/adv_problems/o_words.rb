c4faf8e8e4eef88e3920b463ae956767c7d4901f
LifeH <hwie.zhu@gmail.com>
2019-01-16 14:20:51 -0500

    completed problem

--- ruby-practice/introduction_practice/adv_problems/o_words.rb
+++ ruby-practice/introduction_practice/adv_problems/o_words.rb
@@ -1,5 +1,10 @@
-def o_words(sentence)
+#takes string of sentence
+#returns array of words that contain a certain letter o
+# using .select as array method
+# split sentence into array
 
+def o_words(sentence)
+    return sentence.split.select {|word| word.include?("o")}
 end
 
 print o_words("How did you do that?") #=> ["How", "you", "do"]
