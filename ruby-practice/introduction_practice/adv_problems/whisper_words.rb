12a7d1ac2f9ff9ef7da5f5978578866d4b302615
LifeH <hwie.zhu@gmail.com>
2019-01-16 01:45:03 -0500

    solved problem

--- ruby-practice/introduction_practice/adv_problems/whisper_words.rb
+++ ruby-practice/introduction_practice/adv_problems/whisper_words.rb
@@ -3,7 +3,7 @@
 # add ... to the end of every word
 # using map since this is an array 
 def whisper_words(words)
-    words.map {|word| word.downcase + "..."}
+    return words.map {|word| word.downcase + "..."}
 end
 
 print whisper_words(["KEEP", "The", "NOISE", "down"]) # => ["keep...", "the...", "noise...", "down..."]
