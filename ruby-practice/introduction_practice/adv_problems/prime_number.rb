b540cef044a8169b69d4629dff0c2a2c19f08c5f
LifeH <hwie.zhu@gmail.com>
2019-01-16 17:30:36 -0500

    initial commit

---
+++ ruby-practice/introduction_practice/adv_problems/prime_number.rb
@@ -0,0 +1,10 @@
+def prime?(num)
+
+end
+
+puts prime?(2)  #=> true
+puts prime?(5)  #=> true
+puts prime?(11) #=> true
+puts prime?(4)  #=> false
+puts prime?(9)  #=> false
+puts prime?(-5) #=> false
