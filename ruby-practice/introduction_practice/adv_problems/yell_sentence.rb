31a53f6be6e72982e5679722cdde7fc6975b35ac
LifeH <hwie.zhu@gmail.com>
2019-01-15 22:30:33 -0500

    DRY method

--- ruby-practice/introduction_practice/adv_problems/yell_sentence.rb
+++ ruby-practice/introduction_practice/adv_problems/yell_sentence.rb
@@ -4,11 +4,15 @@
 #map works only on array so split is necessary
 #split sentence to return each word
 def yell_sentence(sent)
-    words = sent.split(" ")
-    new_str = words.map do |word|
-        word.upcase + "!"
-    end
-    return new_str.join(" ")
+    # words = sent.split(" ")
+    # new_str = words.map do |word|
+    #     word.upcase + "!"
+    # end
+    # return new_str.join(" ")
+    
+    #Alternative code: DRY method in a single line by joining the dot methods
+    ret_sent = sent.split.map { |word| word.upcase + "!" }.join(" ")
+    return ret_sent
 end
 
 puts yell_sentence("I have a bad feeling about this") #=> "I! HAVE! A! BAD! FEELING! ABOUT! THIS!"
