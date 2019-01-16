1b20d0d5ecee123bddafe5742fe4899c54f1d50b
LifeH <hwie.zhu@gmail.com>
2019-01-16 17:29:28 -0500

    solved with helper function

--- ruby-practice/introduction_practice/adv_problems/most_vowels.rb
+++ ruby-practice/introduction_practice/adv_problems/most_vowels.rb
@@ -1,5 +1,22 @@
-def most_vowels(sentence)
+#use helper function to take in every word
+#create hash that contains the key as word and value as number of letters
+def vowel_count(word)
+    count = 0
+    vowels = "aeious"
+    word.each_char do |char|
+        if vowels.include?(char)
+            count += 1 
+        end
+    end
+    return count
+end
 
+def most_vowels(sentence)
+    counts = {}
+    words = sentence.split
+    words.each { |word| counts[word] = vowel_count(word) }
+    sorted = counts.sort_by { |k, v| v } #sort by value
+    return sorted[-1][0]
 end
 
 print most_vowels("what a wonderful life") #=> "wonderful"
