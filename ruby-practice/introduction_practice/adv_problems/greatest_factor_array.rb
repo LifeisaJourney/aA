d0e85e9813c36be60dc1e3ec87b838450da32162
LifeH <hwie.zhu@gmail.com>
2019-01-17 14:25:42 -0500

    completed problem

--- ruby-practice/introduction_practice/adv_problems/greatest_factor_array.rb
+++ ruby-practice/introduction_practice/adv_problems/greatest_factor_array.rb
@@ -1,5 +1,23 @@
+# get the range of numbers that can divide into the array as helper function
+# take the helper function only if it is divisible by 2 since it is looking for the greatest factor
 def greatest_factor_array(arr)
+    new_arr = arr.map do |ele|
+        if ele % 2 == 0
+            greatest_factor(ele)
+        else
+            ele
+        end
+    end
+    return new_arr
+end
 
+#need to use reverse_each to go backward since it is searching from the greatest end
+def greatest_factor(nums)
+    (1...nums).reverse_each do |num|
+        if nums % num == 0
+            return num 
+        end
+    end
 end
 
 print greatest_factor_array([16, 7, 9, 14]) # => [8, 7, 9, 7]
