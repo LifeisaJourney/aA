c9310257cd92d42783bf5e82eb5593c5e5676a6f
LifeH <hwie.zhu@gmail.com>
2019-01-16 22:40:17 -0500

    solved problem using helper function

--- ruby-practice/introduction_practice/adv_problems/pick_prime.rb
+++ ruby-practice/introduction_practice/adv_problems/pick_prime.rb
@@ -1,8 +1,22 @@
 #find prime numbers
 #then return prime numbers
 
+def prime?(num)
+    if num < 2 
+        return false
+    end
+
+    (2...num).each do |ele|
+        if num % ele == 0 
+            return false
+        end
+    end
+    return true
+end
+
 def pick_primes(numbers)
-    
+    return numbers.select {|ele| prime?(ele)}
+    #use helper function to find the prime
 end
 
 print pick_primes([2, 3, 4, 5, 6]) #=> [2, 3, 5]
