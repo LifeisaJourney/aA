83f85e17b5a77d92297007a0e514d4b754c2345d
LifeH <hwie.zhu@gmail.com>
2019-01-17 01:11:05 -0500

    hard problem solved

--- ruby-practice/introduction_practice/adv_problems/prime_factors.rb
+++ ruby-practice/introduction_practice/adv_problems/prime_factors.rb
@@ -1,5 +1,24 @@
 def prime_factors(num)
+    prime_array = []
+    (1...num).each do |ele|
+        if num % ele == 0 && prime?(ele)
+            prime_array << ele
+        end
+    end
+    return prime_array
+end
 
+def prime?(nums)
+    if nums < 2
+        return false
+    end
+
+    (2...nums).each do |ele|
+        if nums % ele == 0 
+            return false
+        end
+    end
+    return true
 end
 
 print prime_factors(24) #=> [2, 3]
