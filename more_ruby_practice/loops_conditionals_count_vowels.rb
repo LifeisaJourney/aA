7a1aa147b8af6a80d137465a69ef22546fd81b9d
LifeH <hwie.zhu@gmail.com>
2019-01-19 21:30:08 -0500

    solved problem

--- more_ruby_practice/loops_conditionals_count_vowels.rb
+++ more_ruby_practice/loops_conditionals_count_vowels.rb
@@ -1,5 +1,15 @@
 def count_vowels(word)
-    
+    count = 0
+
+    i=0
+    while i < word.length
+        char = word[i]
+        if char == "a" || char == "e" || char == "i" || char == "o" || char == "u"
+            count+=1
+        end
+        i+=1
+    end
+    return count
 end
 
 puts count_vowels("bootcamp")  # => 3
