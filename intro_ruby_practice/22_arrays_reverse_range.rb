2850ec8241f8e91b714f690991675ac889935e7f
LifeH <hwie.zhu@gmail.com>
2019-01-21 14:51:20 -0500

    solved problem

--- intro_ruby_practice/22_arrays_reverse_range.rb
+++ intro_ruby_practice/22_arrays_reverse_range.rb
@@ -1,5 +1,12 @@
 def reverse_range(min, max)
+    i = max-1
+    new_arr = []
+    while i > min
+        new_arr << i
 
+        i -= 1
+    end
+    return new_arr
 end
   
   print reverse_range(10, 17) # => [16, 15, 14, 13, 12, 11]
