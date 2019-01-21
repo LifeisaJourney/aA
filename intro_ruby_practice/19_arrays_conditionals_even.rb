4eaeb556912b3c2535652a5cccb5da04c57e9c61
LifeH <hwie.zhu@gmail.com>
2019-01-21 12:14:27 -0500

    initial commit

--- intro_ruby_practice/19_arrays_conditionals_even.rb
+++ intro_ruby_practice/19_arrays_conditionals_even.rb
@@ -1,5 +1,14 @@
 def even_nums(max)
+    i = 0
+     new_arr =[]
+     while i < max
+     if i % 2 == 0
+       new_arr << i
+     end
 
+     i+=1
+   end
+ return new_arr
 end
 
 print even_nums(10) # => [0, 2, 4, 6, 8, 10]
