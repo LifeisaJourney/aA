93bd843b52b4368203e5620e83132c9794e6cb8a
LifeH <hwie.zhu@gmail.com>
2019-01-21 12:09:08 -0500

    solved problem

--- intro_ruby_practice/18_arrays loops_iteration.rb
+++ intro_ruby_practice/18_arrays loops_iteration.rb
@@ -1,5 +1,11 @@
 def element_times_index(numbers)
-
+    i = 0
+     new_arr = []
+     while i < numbers.length
+          new_arr << numbers[i] * i
+         i+=1
+   end
+     return new_arr
 end
 
 print element_times_index([4, 7, 6, 5])       # => [0, 7, 12, 15]
