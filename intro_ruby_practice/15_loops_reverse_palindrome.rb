9a8c52be31a293d0903691103cb75cfec489f65b
LifeH <hwie.zhu@gmail.com>
2019-01-20 20:49:06 -0500

    solved problem

--- intro_ruby_practice/15_loops_reverse_palindrome.rb
+++ intro_ruby_practice/15_loops_reverse_palindrome.rb
@@ -1,4 +1,14 @@
 def is_palindrome(word)
+    i = word.length - 1
+    reversed_str = ""
+
+    while i >= 0
+        char = word[i]
+        reversed_str =  reversed_str + char
+    i-=1
+    end
+    p reversed_str
+    return reversed_str == word
 
 end
 
