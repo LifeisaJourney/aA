ab4c6407aef799f72b9ea7513f565b67b34bc5b8
LifeH <hwie.zhu@gmail.com>
2019-01-20 20:43:24 -0500

    solved problem

--- intro_ruby_practice/14_loops_reverse_iteration.rb
+++ intro_ruby_practice/14_loops_reverse_iteration.rb
@@ -1,5 +1,12 @@
 def reverse(word)
-
+    new_str = ""
+       i = word.length-1
+        while i  >= 0
+        char = word[i]
+            new_str << char
+      i-=1
+      end
+    return new_str
 end
    
    puts reverse("cat")          # => "tac"
