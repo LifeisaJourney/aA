73a62280b21c817a852888e3aeb4edb1cbeb69fa
LifeH <hwie.zhu@gmail.com>
2019-01-21 13:04:39 -0500

    solved problem

--- intro_ruby_practice/21_arrays_range_odd_only.rb
+++ intro_ruby_practice/21_arrays_range_odd_only.rb
@@ -1,8 +1,13 @@
 def odd_range(min, max)
-    while min < max 
-
-        min+=1
+    new_arr = []
+    i = min 
+    while i <= max 
+        if i % 2 == 1
+            new_arr<< i
+        end
+        i+=1
     end
+    return new_arr
 end
     
     print odd_range(11, 18) # => [11, 13, 15, 17]
