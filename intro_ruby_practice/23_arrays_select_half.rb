b3f3f63ab4c19a1c75f533e0130487a08c0f5abd
LifeH <hwie.zhu@gmail.com>
2019-01-21 15:25:12 -0500

    solved problem

--- intro_ruby_practice/23_arrays_select_half.rb
+++ intro_ruby_practice/23_arrays_select_half.rb
@@ -1,5 +1,11 @@
 def first_half(array)
-  
+  new_arr = []
+  i = 0
+  while i < array.length / 2.0
+    new_arr << array[i]
+    i+=1
+  end
+  return new_arr
 end
   
   print first_half(["Brian", "Abby", "David", "Ommi"]) # => ["Brian", "Abby"]
