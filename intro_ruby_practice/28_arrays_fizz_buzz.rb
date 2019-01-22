3688c42547ce04fc3435a41b0f908825f28cb3e1
LifeH <hwie.zhu@gmail.com>
2019-01-21 21:06:03 -0500

    solved problem

--- intro_ruby_practice/28_arrays_fizz_buzz.rb
+++ intro_ruby_practice/28_arrays_fizz_buzz.rb
@@ -1,5 +1,13 @@
 def fizz_buzz(max)
-
+  i = 1
+  new_arr = []
+  while i < max 
+    if (i % 4 == 0 || i % 6 == 0) && !( i % 4 ==0 && i % 6 == 0 )
+      new_arr << i
+    end
+    i+=1
+  end
+  return new_arr
 end
 
 print fizz_buzz(20) # => [4, 6, 8, 16, 18]
