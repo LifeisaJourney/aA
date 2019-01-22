56f67df14919b64dd0554342d7f2aebab2ad592f
LifeH <hwie.zhu@gmail.com>
2019-01-21 20:03:17 -0500

    solved problem

--- intro_ruby_practice/26_arrays_.rb
+++ intro_ruby_practice/26_arrays_.rb
@@ -1,5 +1,13 @@
 def select_long_words(words)
-
+  i = 0
+  new_array = []
+  while i < words.length 
+      if words[i].length > 4 
+        new_array << words[i]
+      end
+    i+=1
+  end
+  return new_array
 end
   
   print select_long_words(["what", "are", "we", "eating", "for", "dinner"]) # => ["eating", "dinner"]
