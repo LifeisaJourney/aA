336fa399cf60c0d1f76549dd3a18156d22b22cd2
LifeH <hwie.zhu@gmail.com>
2019-01-22 15:35:24 -0500

    solved problem

--- intro_ruby_practice/32_format_name.rb
+++ intro_ruby_practice/32_format_name.rb
@@ -2,7 +2,13 @@
 # "abc".upcase # => "ABC"
 
 def format_name(str)
-
+  new_str = []
+  splitter = str.split(" ")
+  splitter.each do |word|
+    new_str << word[0].upcase + word[1..-1].downcase
+    #word.capitalize works too
+  end
+  return new_str.join(" ")
 end
 
 puts format_name("chase WILSON") # => "Chase Wilson"
