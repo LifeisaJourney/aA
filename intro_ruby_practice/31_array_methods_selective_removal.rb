9d0759f14be90bae9c7a19927e4932356b1dd413
LifeH <hwie.zhu@gmail.com>
2019-01-22 13:56:42 -0500

    solved problem with helper function

--- intro_ruby_practice/31_array_methods_selective_removal.rb
+++ intro_ruby_practice/31_array_methods_selective_removal.rb
@@ -1,6 +1,27 @@
 def abbreviate_sentence(sent)
-
+  new_str = []
+  splitter=sent.split(" ")
+  splitter.each do |word|
+    if word.length > 4
+      new_str << abbreviate_word(word)
+    end
+  end
+  return new_str.join(" ")
+  # sent.each_char do |word|
+  #   p word
+  # end
 end
 
+def abbreviate_word(word)
+  vowels = "aeiou"
+  new_str = ""
+  word.each_char do |char|
+    if !vowels.include?(char)
+    new_str << char
+    end
+  end
+  return new_str
+end
+puts abbreviate_word("test")
 puts abbreviate_sentence("follow the yellow brick road") # => "fllw the yllw brck road"
 puts abbreviate_sentence("what a wonderful life")        # => "what a wndrfl life"
