7984aa510d1b8f348d3b7e52ee1f674b96501dab
LifeH <hwie.zhu@gmail.com>
2019-01-24 01:18:42 -0500

    initial commit

---
+++ intro_ruby_practice/36_array_methods_reverse_word.rb
@@ -0,0 +1,6 @@
+def reverse_words(sent)
+
+end
+
+puts reverse_words('keep coding') # => 'peek gnidoc'
+puts reverse_words('simplicity is prerequisite for reliability') # => 'yticilpmis si etisiuqererp rof ytilibailer'
