d71de6be3ea3d65998c1a54f994add9aa034444b
LifeH <hwie.zhu@gmail.com>
2019-01-24 00:32:41 -0500

    initial commit

---
+++ intro_ruby_practice/35_array_methods_each.rb
@@ -0,0 +1,9 @@
+def array_translate(array)
+
+end
+
+print array_translate(["Cat", 2, "Dog", 3, "Mouse", 1]); # => "CatCatDogDogDogMouse"
+puts
+
+print array_translate(["red", 3, "blue", 1]); # => "redredredblue"
+puts
