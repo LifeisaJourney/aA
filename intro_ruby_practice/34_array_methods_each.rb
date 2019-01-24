120e87c28ef6b468f42724d64905497add838cfd
LifeH <hwie.zhu@gmail.com>
2019-01-24 00:23:36 -0500

    solved problem

--- intro_ruby_practice/34_array_methods_each.rb
+++ intro_ruby_practice/34_array_methods_each.rb
@@ -4,7 +4,26 @@
 # - contains exactly one . after the @
 
 def is_valid_email(str)
-	
+    splitter = str.split("@")
+    if splitter.length!=2
+        return false 
+    end 
+
+    first = splitter[0]
+    second = splitter[1]
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    first.each_char do |char|
+        if !alphabet.include?(char)
+            return false
+        end
+        
+    end
+    if second.split(".").length == 2
+        return true
+    else 
+        return false
+    end
 end
 
 puts is_valid_email("abc@xy.z")         # => true
