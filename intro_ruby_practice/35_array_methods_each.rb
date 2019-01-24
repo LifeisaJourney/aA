59b141ec24c44c043ea4cdb32dd926534691c33e
LifeH <hwie.zhu@gmail.com>
2019-01-24 00:45:27 -0500

    solved problem

--- intro_ruby_practice/35_array_methods_each.rb
+++ intro_ruby_practice/35_array_methods_each.rb
@@ -1,5 +1,13 @@
 def array_translate(array)
-
+  i=0
+  new_str = ""
+  while i < array.length
+    word = array[i]
+    times = array[i+1]
+    times.times {new_str << word}
+    i+=2
+  end
+  return new_str
 end
 
 print array_translate(["Cat", 2, "Dog", 3, "Mouse", 1]); # => "CatCatDogDogDogMouse"
