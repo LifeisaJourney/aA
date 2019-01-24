4a6ecd483956237d68ac81a5d65abeabf48bcbe3
LifeH <hwie.zhu@gmail.com>
2019-01-23 23:48:44 -0500

    solved problem with helper function

--- intro_ruby_practice/33_array_methods_each.rb
+++ intro_ruby_practice/33_array_methods_each.rb
@@ -6,8 +6,27 @@
 # "a".upcase # => "A"
 
 def is_valid_name(str)
+  splitter = str.split(" ")
+  if splitter.length < 2
+    return false
+  end
 
+  splitter.each do |word|
+    if !icap(word)
+      return false
+    end
+  end
+  return true
 end
+
+  def icap(ele)
+    if ele[0]==ele[0].upcase && ele[1..-1] == ele[1..-1].downcase 
+      return true
+    else
+      return false
+  end
+
+  end
 
 puts is_valid_name("Kush Patel")       # => true
 puts is_valid_name("Daniel")           # => false
