28e16ac5636cae8e60d7c600be40923a9965ef05
LifeH <hwie.zhu@gmail.com>
2019-01-25 15:50:57 -0500

    initial commit

---
+++ intro_ruby_practice/50_hashes_select_upcase_keys.rb
@@ -0,0 +1,8 @@
+#Write a method select_upcase_keys that takes in a hash and returns a new hash containing key-value pairs of the original hash that had uppercase keys. You can assume that the keys will always be strings.
+
+def element_count(arr)
+
+end
+
+puts element_count(["a", "b", "a", "a", "b"]) #=> {"a"=>3, "b"=>2}
+puts element_count(["red", "red", "blue", "green"]) #=> {"red"=>2, "blue"=>1, "green"=>1}
