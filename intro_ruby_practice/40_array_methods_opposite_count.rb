0a7bc1d23fefd5e5ca3130e5b60c844b8a43d358
LifeH <hwie.zhu@gmail.com>
2019-01-25 17:19:54 -0500

    finished problem

--- intro_ruby_practice/40_array_methods_opposite_count.rb
+++ intro_ruby_practice/40_array_methods_opposite_count.rb
@@ -1,7 +1,17 @@
 #Write a method opposite_count that takes in an array of unique numbers. The method should return the number of pairs of elements that sum to 0.
 
 def opposite_count(nums)
+  count = 0
+  nums.each.with_index do |ele1, i1|
+    nums.each.with_index do |ele2, i2|
+      if i2>i1 && ele2 + ele1==0
+        count+=1
 
+      end
+
+    end
+  end
+  return count
 end
 
 puts opposite_count([ 2, 5, 11, -5, -2, 7 ]) # => 2
