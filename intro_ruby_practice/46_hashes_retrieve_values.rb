7d5632f17f98be7b18c6175f9a1b4c6a8d819c0f
LifeH <hwie.zhu@gmail.com>
2019-01-26 18:35:52 -0500

    solved problem

--- intro_ruby_practice/46_hashes_retrieve_values.rb
+++ intro_ruby_practice/46_hashes_retrieve_values.rb
@@ -1,7 +1,9 @@
 #Write a method retrieve_values that takes in two hashes and a key. The method should return an array containing the values from the two hashes that correspond with the given key.
 
 def retrieve_values(hash1, hash2, key)
-
+    val1 = hash1[key]
+    val2 = hash2[key] 
+    return [val1, val2]
 end
 
 
