1fec63ab5e03435e1f6ac52559aa61847b54f97b
LifeH <hwie.zhu@gmail.com>
2019-01-26 13:44:26 -0500

    solved problem

--- intro_ruby_practice/41_array_methods_2D_sum.rb
+++ intro_ruby_practice/41_array_methods_2D_sum.rb
@@ -1,7 +1,13 @@
 #Write a method two_d_Sum that takes in a two dimensional array and returns the sum of all elements in the array.
 
 def two_d_sum(arr)
-
+  count = 0
+  arr.each do |char|
+    char.each do |ele|
+      count += ele
+    end
+  end
+  return count
 end
 
 array_1 = [
