652e08624315dd7e6fc5c8f39ce4e0a4e432886a
LifeH <hwie.zhu@gmail.com>
2019-01-26 22:23:09 -0500

    solved problem

--- intro_ruby_practice/50_hashes_select_upcase_keys.rb
+++ intro_ruby_practice/50_hashes_select_upcase_keys.rb
@@ -1,7 +1,13 @@
 #Write a method select_upcase_keys that takes in a hash and returns a new hash containing key-value pairs of the original hash that had uppercase keys. You can assume that the keys will always be strings.
 
 def select_upcase_keys(hash)
-
+	new_hash = {}
+  	hash.map do |k, v|
+      if k==k.upcase
+        new_hash[k] = v
+      end
+    end
+  return new_hash
 end
 
 print select_upcase_keys({"make"=> "Tesla", "MODEL"=> "S", "Year"=> 2018, "SEATS"=> 4}) # => {"MODEL"=>"S", "SEATS"=>4}
