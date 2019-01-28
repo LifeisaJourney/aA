18a9cee1cf365bd6498ae8825a94f6f6af872928
LifeH <hwie.zhu@gmail.com>
2019-01-28 18:12:49 -0500

    solved problem

--- intro_ruby_practice/59_adv_whisper_words.rb
+++ intro_ruby_practice/59_adv_whisper_words.rb
@@ -1,7 +1,11 @@
 #Write a method whisper_words that takes in an array of words and returns a new array containing a whispered version of each word. See the examples. 
 
 def whisper_words(words)
-
+    new_arr = []
+    words.map do |ele|
+        new_arr << ele.downcase + "..."
+    end
+ return new_arr
 end
 
 print whisper_words(["KEEP", "The", "NOISE", "down"]) # => ["keep...", "the...", "noise...", "down..."]
