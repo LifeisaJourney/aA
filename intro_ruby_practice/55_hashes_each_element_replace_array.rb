028055a4af0565afb7d523810572b0ca52937c3c
LifeH <hwie.zhu@gmail.com>
2019-01-28 14:00:09 -0500

    completed problem

--- intro_ruby_practice/55_hashes_each_element_replace_array.rb
+++ intro_ruby_practice/55_hashes_each_element_replace_array.rb
@@ -1,7 +1,15 @@
 #Write a method element_replace that takes in an array and a hash. The method should return a new array where elements of the original array are replaced with their corresponding values in the hash.
 
 def element_replace(arr, hash)
-    
+    new_arr = []
+    arr.each do |ele|
+        if hash.has_key?(ele)
+            new_arr << hash[ele]
+        else 
+            new_arr << ele
+        end
+    end
+    return new_arr
 end
 
 arr1 = ["LeBron James", "Lionel Messi", "Serena Williams"]
