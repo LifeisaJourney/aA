e2b74ceb2034dda0048b91feb8c2699a311f1d68
LifeH <hwie.zhu@gmail.com>
2019-01-27 22:19:30 -0500

    solved problem

--- intro_ruby_practice/52_hashes_frequent_letters.rb
+++ intro_ruby_practice/52_hashes_frequent_letters.rb
@@ -1,7 +1,16 @@
 #Write a method frequent_letters that takes in a string and returns an array containing the characters that appeared more than twice in the string.
 
 def frequent_letters(string)
+    counter = Hash.new(0)
+    string.each_char {|ele| counter[ele]+=1}
 
+    new_arr = []
+    counter.each do |char, num|
+        if num > 2
+            new_arr << char
+        end
+    end
+   return new_arr
 end
 
 print frequent_letters('mississippi') #=> ["i", "s"]
