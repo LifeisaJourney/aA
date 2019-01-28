82385e7199a857015e4010e9e37e6ac03828f96a
LifeH <hwie.zhu@gmail.com>
2019-01-28 18:46:08 -0500

    added two solutions to the problem

--- intro_ruby_practice/60_adv_o_words.rb
+++ intro_ruby_practice/60_adv_o_words.rb
@@ -1,7 +1,19 @@
 #Write a method o_words that takes in a sentence string and returns an array of the words that contain an "o". Use select in your solution!
 
 def o_words(sentence)
+    #return sentence.split(" ").select {|ele| ele.include?("o")}
 
+    #alternative solution
+    new_arr = []
+    splitter= sentence.split(" ")
+    splitter.map do |word|
+        word.each_char do |char|
+            if word[char] == "o"
+                new_arr << word
+            end
+        end
+    end
+    return new_arr
 end
 
 print o_words("How did you do that?") #=> ["How", "you", "do"]
