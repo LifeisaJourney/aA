ebbfcc46892479ed48862c8fcef7ad992eb8f084
LifeH <hwie.zhu@gmail.com>
2019-01-28 15:16:35 -0500

    two different solutions, each vs map

--- intro_ruby_practice/57_adv_access_map_key.rb
+++ intro_ruby_practice/57_adv_access_map_key.rb
@@ -1,11 +1,15 @@
 #Write a method map_by_key that takes in an array of hashes and a key string. The method should returns a new array containing the values from each hash for the given key.
 
 def map_by_key(arr, key)
-  new_arr = []
-  arr.each do |ele| 
-    new_arr << ele[key]
-  end
-  return new_arr
+  #alternative solution
+  
+  # new_arr = []
+  # arr.map do |ele| 
+  #   new_arr << ele[key]
+  # end
+  # return new_arr
+  
+  return arr.map {|ele| ele[key] }
 end
 
 locations = [
