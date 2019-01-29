36d23e77585dddffb7bd639b6f508796b2b16775
LifeH <hwie.zhu@gmail.com>
2019-01-28 23:47:01 -0500

    solved problem

--- intro_ruby_practice/63_adv_prime_num.rb
+++ intro_ruby_practice/63_adv_prime_num.rb
@@ -1,7 +1,16 @@
 #Write a method prime? that takes in a number and returns a boolean, indicating whether the number is prime. A prime number is only divisible by 1 and itself.
 
 def prime?(num)
+  if num < 2
+    return false
+  end
 
+  (2...num).each do |factor|
+    if num % factor == 0 
+      return false
+    end
+  end
+  return true
 end
 
 puts prime?(2)  #=> true
