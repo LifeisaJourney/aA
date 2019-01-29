b240d10f883963c76bb9dfae7fd6b92ff0e60994
LifeH <hwie.zhu@gmail.com>
2019-01-28 20:50:36 -0500

    solved problem with helper function

--- intro_ruby_practice/r62_adv_most_vowels.rb
+++ intro_ruby_practice/r62_adv_most_vowels.rb
@@ -1,7 +1,24 @@
 #Write a method most_vowels that takes in a sentence string and returns the word of the sentence that contains the most vowels.
 
 def most_vowels(sentence)
+    count = {}
+    splitter =sentence.split(" ")
+    splitter.each {|ele| count[ele] = vowel_count(ele)}
 
+    sorted = count.sort_by{|k, v| v}
+    return sorted[-1][0]
+
+end
+
+def vowel_count(word)
+    count = 0
+    vowels = "aeiou"
+    word.each_char do |char|
+        if vowels.include?(char)
+            count+=1
+        end
+    end
+    return count
 end
 
 print most_vowels("what a wonderful life") #=> "wonderful"
