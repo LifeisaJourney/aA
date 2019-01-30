d5dcf98447010603d7d82cd555569cb953ea25ec
LifeH <hwie.zhu@gmail.com>
2019-01-30 14:34:29 -0500

    renamed

--- intro_ruby_practice/66_adv_map_h_fxn_greatest_factor_array.rb
+++ intro_ruby_practice/66_adv_map_h_fxn_greatest_factor_array.rb
@@ -4,6 +4,7 @@ def greatest_factor_array(arr)
 
 end
 
+
 print greatest_factor_array([16, 7, 9, 14]) # => [8, 7, 9, 7]
 puts
 print greatest_factor_array([30, 3, 24, 21, 10]) # => [15, 3, 12, 21, 5]
