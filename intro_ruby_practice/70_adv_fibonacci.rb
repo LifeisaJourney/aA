e640a6db53df3e6e754793e744da5515518f3e09
LifeH <hwie.zhu@gmail.com>
2019-01-31 17:42:15 -0500

    finished problem

--- intro_ruby_practice/70_adv_fibonacci.rb
+++ intro_ruby_practice/70_adv_fibonacci.rb
@@ -1,7 +1,21 @@
 #The fibonacci sequence is a sequence of numbers whose first and second elements are 1. To generate further elements of the sequence we take the sum of the previous two elements. For example the first 6 fibonacci numbers are 1, 1, 2, 3, 5, 8. Write a method fibonacci that takes in a number length and returns the fibonacci sequence up to the given length.
 
 def fibonacci(length)
+    if length == 0
+        return []
+    elsif length == 1
+        return [1]
+    end
 
+    seq = [1, 1]
+
+    while seq.length < length
+        l = seq[-1]
+        f = seq[-2]
+        sum = f + l 
+        seq << sum 
+    end
+    return seq
 end
 
 print fibonacci(0) # => []
