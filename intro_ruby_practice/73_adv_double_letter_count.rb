c8c58daf14916ac184fdb6a2c181a6953d4e0e9d
LifeH <hwie.zhu@gmail.com>
2019-02-01 17:19:50 -0500

    solved problem

--- intro_ruby_practice/73_adv_double_letter_count.rb
+++ intro_ruby_practice/73_adv_double_letter_count.rb
@@ -1,7 +1,14 @@
 #Write a method that takes in a string and returns the number of times that the same letter repeats twice in a row.
 
 def double_letter_count(string)
+    counter = 0 
 
+    string.each_char.with_index do |ele, i|
+        if string[i] == string[i+1] 
+            counter+=1
+        end
+    end
+    return counter
 end
 
 puts double_letter_count("the jeep rolled down the hill") #=> 3
