9a5bf0a3c3912e2d2262667107316096788050f2
LifeH <hwie.zhu@gmail.com>
2019-02-01 15:26:12 -0500

    solved problem

--- intro_ruby_practice/72_adv_vowel_cipher.rb
+++ intro_ruby_practice/72_adv_vowel_cipher.rb
@@ -1,7 +1,18 @@
 #Write a method vowel_cipher that takes in a string and returns a new string where every vowel becomes the next vowel in the alphabet.
 
 def vowel_cipher(string)
+    vowels = "aeiou"
 
+    new_str = string.split("").map do |ele|
+        if vowels.include?(ele)
+            old_index = vowels.index(ele)
+            new_index = (old_index + 1) % vowels.length
+            vowels[new_index]
+        else
+            ele
+        end
+    end
+    return new_str.join("")
 end
 
 puts vowel_cipher("bootcamp") #=> buutcemp
