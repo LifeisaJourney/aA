7bdcfd8f803827daa4b2da378bc556a0f5b8d1a0
LifeH <hwie.zhu@gmail.com>
2019-02-03 17:42:32 -0500

    solved problem with solution 1

--- intro_ruby_practice/77_adv_hash_anagrams.rb
+++ intro_ruby_practice/77_adv_hash_anagrams.rb
@@ -2,6 +2,17 @@
 
 def anagrams?(word1, word2)
 
+  # Solution 1:
+  #   word1.each_char do |ele1|
+  #     word2.each_char do |ele2|
+  #       if ele1==ele2 && word1.length == word2.length
+  #         return true
+  #       end
+  #     end
+  #   end
+  # return false
+
+  #   Solution 2:
 end
 
 puts anagrams?("cat", "act")          #=> true
