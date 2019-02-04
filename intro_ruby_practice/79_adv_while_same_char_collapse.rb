2bb081cee974e03342205299c936dd55a07cd454
LifeH <hwie.zhu@gmail.com>
2019-02-04 14:22:16 -0500

    renamed file and solved problem

--- intro_ruby_practice/79_adv_while_same_char_collapse.rb
+++ intro_ruby_practice/79_adv_while_same_char_collapse.rb
@@ -1,6 +1,26 @@
 #Write a method same_char_collapse that takes in a string and returns a collapsed version of the string. To collapse the string, we repeatedly delete 2 adjacent characters that are the same until there are no such adjacent characters. If there are multiple pairs that can be collapsed, delete the leftmost pair. For example, we take the following steps to collapse "zzzxaaxy": zzzxaaxy -> zxaaxy -> zxxy -> zy
 
 def same_char_collapse(str)
+    reducible = true
+
+    while reducible
+
+        new_arr = str.split("")
+        reducible = false
+
+        new_arr.each.with_index do |char, i|
+            if new_arr[i] == new_arr[i+1]
+                new_arr[i] = ""
+                new_arr[i+1] = ""
+                reducible = true
+                break
+            end
+        end
+        
+        str = new_arr.join("")
+    end
+    return str
+
 
 end
 
