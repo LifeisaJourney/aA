3e2b685c5c7a66baee3f9e47689912d1a77695ed
LifeH <hwie.zhu@gmail.com>
2019-02-04 01:11:31 -0500

    solved problem using helper function

--- intro_ruby_practice/78_adv_h_fxn_consonant_cancel.rb
+++ intro_ruby_practice/78_adv_h_fxn_consonant_cancel.rb
@@ -2,8 +2,21 @@
 
 
 def consonant_cancel(sentence)
-
+  new_arr = sentence.split(" ")
+  new_str = new_arr.map do |char|
+    remove(char)
+  end
+  new_str.join(" ")
 end
 
+def remove(word)
+  vowels = "aeiou"
+  word.each_char.with_index do |char, i|
+    if vowels.include?(char)
+      return word[i..-1]
+    end
+  end
+end
+p remove("rabiit")
 puts consonant_cancel("down the rabbit hole") #=> "own e abbit ole"
 puts consonant_cancel("writing code is challenging") #=> "iting ode is allenging"
