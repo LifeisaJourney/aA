f67aa2c5e957cd1ab152dc526e6968ef8934a26f
LifeH <hwie.zhu@gmail.com>
2019-03-28 16:32:39 -0400

    tests created for #multiply and multi

--- alpha_curriculum/08 RSPEC/rspec_project/spec/02_calculator_spec.rb
+++ alpha_curriculum/08 RSPEC/rspec_project/spec/02_calculator_spec.rb
@@ -80,8 +80,12 @@ end
 # write tests and code for the following:
 
 describe "#multiply" do
-  it "multiplies two numbers"
-  it "multiplies several numbers"
+  it "multiplies two numbers" do 
+    expect(multiply(2, 7)).to eq(14)
+  end
+  it "multiplies several numbers" do 
+    expect(multi(7, 3)).to eq(21)
+  end
 end
 
 describe "#power" do
