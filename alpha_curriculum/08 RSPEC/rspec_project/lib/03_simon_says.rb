01e7c97ebf5ffce96d903c551e2363478d640f97
LifeH <hwie.zhu@gmail.com>
2019-03-30 17:58:13 -0400

    revised syntax

---
+++ alpha_curriculum/08 RSPEC/rspec_project/lib/03_simon_says.rb
@@ -0,0 +1,13 @@
+def echo(word)
+    empty_str = ""
+    return empty_str << word
+
+end
+
+# def shout(word)
+#     if word == "hello"
+#         return word.upcase
+#     elsif word=="bye"
+#        return  word.upcase
+#     end
+# end
