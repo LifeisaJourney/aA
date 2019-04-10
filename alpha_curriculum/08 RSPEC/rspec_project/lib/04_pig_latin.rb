4abb8dbdfa90ddd567ff3c5ae92ab6cbf5c92802
LifeH <hwie.zhu@gmail.com>
2019-04-10 19:38:14 -0400

    part1 for 04.rb solved

--- alpha_curriculum/08 RSPEC/rspec_project/lib/04_pig_latin.rb
+++ alpha_curriculum/08 RSPEC/rspec_project/lib/04_pig_latin.rb
@@ -1,11 +1,13 @@
 def translate(word)
   vowels = ["a", "e", "i", "o", "u"]
+  new_str = ""
   new_arr = word.split(" ")
   new_arr.each do |ele|
-    
     if vowels.include?(ele[0])
-      ele = ele + "ay"
+      new_str << ele + "ay"
+      p new_str
     end
-
   end
+  return new_str
 end
+
