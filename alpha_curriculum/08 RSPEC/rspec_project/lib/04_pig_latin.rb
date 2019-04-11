38908f1fec2c6e34aa89b7033ef223f9079803f2
LifeH <hwie.zhu@gmail.com>
2019-04-11 19:48:46 -0400

    need to refractor for big O

--- alpha_curriculum/08 RSPEC/rspec_project/lib/04_pig_latin.rb
+++ alpha_curriculum/08 RSPEC/rspec_project/lib/04_pig_latin.rb
@@ -1,12 +1,25 @@
 def translate(word)
+  # vowels = "aeiou"
   vowels = ["a", "e", "i", "o", "u"]
   new_str = ""
   new_arr = word.split(" ")
-  new_arr.each do |ele|
+  i=0
+  new_arr.map do |ele|
     if vowels.include?(ele[0])
       new_str << ele + "ay"
-      p new_str
+      next
     end
+    unless vowels.include?(ele[0] == vowels)
+      new_str << ele[i+1..-1] + ele[i] + "ay"
+      next
+    #   new_str << ele + "ay"
+    # elsif 
+    #   new_str << ele[1..-1] + ele[0] + "ay"
+
+    # ele.each_char do |val|
+    #   p val[0]
+    end
+    
   end
   return new_str
 end
