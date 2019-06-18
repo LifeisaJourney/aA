4aa9a62ccd41626c7d4f1bb1d4ff03012c5cd399
LifeH <hwie.zhu@gmail.com>
2019-06-18 16:11:01 -0400

    initial commit on enumerables project exercise

--- alpha_curriculum/08 RSPEC/rspec_project/lib/04_pig_latin.rb
+++ alpha_curriculum/08 RSPEC/rspec_project/lib/04_pig_latin.rb
@@ -4,25 +4,54 @@ def translate(word)
   new_str = ""
   new_arr = word.split(" ")
   i=0
-  new_arr.map do |ele|
-    if vowels.include?(ele[0])
-      new_str << ele + "ay"
-      next
-    end
-    unless vowels.include?(ele[0])
-      new_str << ele[i+1..-1] + ele[i] + "ay"
-      next
-      i+=1
-      
+  # new_arr.map do |ele|
+    # if vowels.include?(ele[0])
     #   new_str << ele + "ay"
-    # elsif 
-    #   new_str << ele[1..-1] + ele[0] + "ay"
+    #   next
+    # end
+    # unless vowels.include?(ele[0])
+    #   new_str << ele[i+1..-1] + ele[i] + "ay"
+    #   next
+    #   i+=1
+    # end
+  # end
 
-    # ele.each_char do |val|
-    #   p val[0]
-    end
+    # while i < word.length
+    #   first_letter = true
+    #   new_arr.map do |ele|
+    #     p ele
+    #     vowel_first = vowels.include?(ele[0])
+    #     if vowel_first
+    #       new_str << ele + "ay"
+    #       next
+    #     end
+    #   end
+    #   i+=1
+    # end
+
+    new_arr.map do |ele|
+      vowel_first = vowels.include?(ele[0])
+      while i < word.length
+        p vowel_first
+        p word.length
+        p ele[i]
+        if vowel_first
+          new_str << ele + "ay"
+          p new_str
+          break
+        end
+        # elsif
+        #   new_str << ele[i..-1] + ele[i] + "ay"
+        #   p new_str
+        #   i+=1 
+        #   break
+
+
+      i+=1
+      # break
+      end
     
-  end
+    end
   return new_str
 end
 
