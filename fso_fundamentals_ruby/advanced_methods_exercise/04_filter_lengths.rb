b7a76684fb5f275e9b903903949c958c7f720e13
LifeH <hwie.zhu@gmail.com>
2019-08-10 19:19:51 -0400

    refactored 04

--- fso_fundamentals_ruby/advanced_methods_exercise/04_filter_lengths.rb
+++ fso_fundamentals_ruby/advanced_methods_exercise/04_filter_lengths.rb
@@ -12,6 +12,10 @@ def filter_lengths(strings, length = 5)
 end
 
 
+def filter_lengths(strings, length = 5)
+    strings.select { |el| el.length >= length }
+end
+
 
 p filter_lengths(["pear", "dragonfruit", "fig", "clementine"], 4)   # => ["pear", "dragonfruit", "clementine"]
 p filter_lengths(["pear", "dragonfruit", "fig", "clementine"])      # => ["dragonfruit", "clementine"]
