9fe4fc0d28b7463fd66b0509e2dbae42ca818b85
LifeH <hwie.zhu@gmail.com>
2019-08-10 18:28:13 -0400

    01 solved

--- fso_fundamentals_ruby/advanced_methods_exercise/01_coprime.rb
+++ fso_fundamentals_ruby/advanced_methods_exercise/01_coprime.rb
@@ -2,7 +2,14 @@
 # The method should return true if the only common divisor between the two numbers is 1.
 # The method should return false otherwise. For example coprime?(25, 12) is true because
 # 1 is the only number that divides both 25 and 12.
-
+def coprime?(num_1, num_2)
+    (2..num_1).each do |divisor|
+        if num_1%divisor == 0 && num_2%divisor == 0
+            return false
+        end
+    end
+    true
+end
 
 p coprime?(25, 12)    # => true
 p coprime?(7, 11)     # => true
