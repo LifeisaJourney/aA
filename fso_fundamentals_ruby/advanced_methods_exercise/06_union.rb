730910fcaaf272ac5d99ebc6311ebc048fa7b54e
LifeH <hwie.zhu@gmail.com>
2019-08-13 15:19:56 -0400

    finished problem set

--- fso_fundamentals_ruby/advanced_methods_exercise/06_union.rb
+++ fso_fundamentals_ruby/advanced_methods_exercise/06_union.rb
@@ -1,5 +1,12 @@
 # Write a method, union, that accepts any number of arrays as arguments.
 # The method should return an array containing all elements of the given arrays.
+def union(*arr)
+    arr.flatten
+end
+
+def union(*arr)
+    arr.inject {|acc, el| acc + el}
+end
 
 
 p union(["a", "b"], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
