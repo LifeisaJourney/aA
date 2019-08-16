c2f047feda6143e1dca15d412fd74c39a652d1e9
LifeH <hwie.zhu@gmail.com>
2019-08-16 13:29:51 -0400

    finished problem sets

--- fso_fundamentals_ruby/algorithmic_strategies_exercise/04_peak_finder.rb
+++ fso_fundamentals_ruby/algorithmic_strategies_exercise/04_peak_finder.rb
@@ -2,7 +2,21 @@
 # The method should return an array containing all of "peaks" of the array.
 # An element is considered a "peak" if it is greater than both it's left and right neighbor.
 # The first or last element of the array is considered a "peak" if it is greater than it's one neighbor.
-
+def peak_finder(arr)
+  peaks = []
+  arr.each_with_index do |ele, i|
+    left = arr[i-1]
+    right = arr[i+1]
+    if i == 0 && ele > right 
+      peaks << ele 
+    elsif i==arr.length-1 && ele > left
+      peaks << ele
+    elsif ele > left && ele > right 
+      peaks << ele 
+    end
+  end
+  peaks
+end
 
 p peak_finder([1, 3, 5, 4])         # => [5]
 p peak_finder([4, 2, 3, 6, 10])     # => [4, 10]
