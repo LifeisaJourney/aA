366eda11aa6d4395ee0055d23c24ab9716540215
LifeH <hwie.zhu@gmail.com>
2019-08-31 16:34:35 -0400

    solved part 2 of blocks

---
+++ fso_fundamentals/blocks_project/lib/part_2.rb
@@ -0,0 +1,19 @@
+def all_words_capitalized?(arr)
+    arr.all? {|el| el.capitalize == el}
+end
+
+
+def no_valid_url?(urls)
+    valid_urls = ['.com', '.net', '.io', '.org']
+    #Urls is an Array Enumerable that turns it into string class url of every site
+    urls.none? do |url|
+        #Valid_urls is an Array Enumerable that turns |ending| into string of each endings
+        valid_urls.any? {|ending| url.end_with?(ending)}
+    end
+end
+
+
+def any_passing_students?(hash)
+    #access the key :grades within the hash, then find the average of it
+    hash.any? {|el| (el[:grades].sum)/(el[:grades].count) >= 75}
+end
