a8e6b43977dbb431ad049abe474acb9ae5a7b904
LifeH <hwie.zhu@gmail.com>
2019-10-06 14:54:01 -0400

    finished part 1

--- fso_fundamentals/class_project exercise/bootcamp_project/lib/bootcamp.rb
+++ fso_fundamentals/class_project exercise/bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,46 @@
 class Bootcamp
-  
+  def initialize(name,slogan,student_capacity)
+    @name = name
+    @slogan = slogan
+    @student_capacity = student_capacity
+    @teachers = []
+    @students = []
+    @grades = Hash.new {|hash, k| hash[k] = []}
+  end
+
+  def name
+    @name 
+  end
+
+  def slogan
+    @slogan
+  end
+
+  def student_capacity
+    @student_capacity 
+  end
+
+  def teachers
+    @teachers 
+  end
+
+  def students
+    @students 
+  end
+
+  def hire(teacher)
+    @teachers<<teacher
+  end
+
+  def enroll(student)
+    if student_capacity > students.length
+      @students << student
+      return true
+    end
+    false
+  end
+
+  def enrolled?(student)
+    @students.include?(student)
+  end
 end
