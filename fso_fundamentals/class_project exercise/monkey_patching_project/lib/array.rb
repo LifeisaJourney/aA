a25cfe0b52502ec5f4664aec0160f86d19c39f44
LifeH <hwie.zhu@gmail.com>
2019-10-07 18:00:33 -0400

    finished part 1

--- fso_fundamentals/class_project exercise/monkey_patching_project/lib/array.rb
+++ fso_fundamentals/class_project exercise/monkey_patching_project/lib/array.rb
@@ -1,4 +1,44 @@
 # Monkey-Patch Ruby's existing Array class to add your own custom methods
 class Array
-  
+  def span
+    sorted = self.sort
+    if self.length > 0 
+      difference = sorted.last - sorted.first
+      return difference
+    end
+    nil
+  end
+
+  def average 
+    sum = 0
+    average=0
+    if self.length > 0
+      self.each do |el|
+        sum+=el
+      end
+      average = sum.to_f/self.length
+      return average
+    end
+    nil 
+  end
+
+  def median
+    return nil if self.length == 0
+    
+    sorted = self.sort
+    middle = sorted.length/2
+    if sorted.length % 2 == 1 
+      return sorted[middle]
+    elsif sorted.length % 2 == 0
+      left =  (sorted[middle])
+      right =sorted[middle-1]
+      return avg = (left + right)/2.0
+    end
+  end
+    
+  def counts
+    count = Hash.new(0)
+    self.each  {|ele| count[ele]+=1}
+    count
+  end
 end
