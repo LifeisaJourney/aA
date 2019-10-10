84af2c35249b9d091d751b506aa5047d37fd9e9a
LifeH <hwie.zhu@gmail.com>
2019-10-10 02:22:07 -0400

    finished guessing game

--- fso_fundamentals/class_project exercise/guessing_game_project/lib/guessing_game.rb
+++ fso_fundamentals/class_project exercise/guessing_game_project/lib/guessing_game.rb
@@ -1,3 +1,33 @@
 class GuessingGame
+  def initialize(min, max)
+    @secret_num = rand(0..100)
+    @num_attempts = 0
+    @game_over = false
+  end
 
+  def num_attempts
+     @num_attempts
+  end
+
+  def game_over?
+     @game_over
+  end
+
+  def check_num(num)
+    @num_attempts+=1
+    if num > @secret_num
+      print 'too big'
+    elsif num < @secret_num
+      print 'too small'
+    else num == @secret_num
+      @game_over = true
+      print 'you win'
+    end
+  end
+
+  def ask_user
+    print 'enter a number'
+    response = gets.chomp.to_i
+    check_num(response)
+  end
 end
