caf67c5c1c36693b808b3a2110927ed2154de8f7
LifeH <hwie.zhu@gmail.com>
2019-10-10 19:49:47 -0400

    finished part 1

--- fso_fundamentals/class_project exercise/hangman_project/lib/hangman.rb
+++ fso_fundamentals/class_project exercise/hangman_project/lib/hangman.rb
@@ -1,4 +1,51 @@
 class Hangman
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
 
+  def self.random_word
+    DICTIONARY.sample
+  end
+
+  def initialize 
+    @secret_word = Hangman.random_word
+    @guess_word = Array.new(@secret_word.length, "_")
+    @attempted_chars = []
+    @remaining_incorrect_guesses = 5
+  end
+
+  def guess_word
+    @guess_word
+  end
+
+  def attempted_chars
+    @attempted_chars
+  end
+  def remaining_incorrect_guesses
+    @remaining_incorrect_guesses
+  end
+
+  def already_attempted?(char)
+    if @attempted_chars.include?(char)
+      return true
+    else
+      return false
+    end
+  end
+
+  def get_matching_indices(char)
+    matching_indices = []
+      @secret_word.each_char.with_index do |k, i|
+        if char == k
+          matching_indices << i 
+        end
+      end
+    matching_indices
+  end
+
+  def fill_indices(char, arr)
+   arr.each do |el|
+    @guess_word[el] = char
+   end
+
+  end
 end
+
