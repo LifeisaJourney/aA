dae275ba05fac6ae67b73f9b83d19b01fe585f77
LifeH <hwie.zhu@gmail.com>
2019-10-11 13:11:28 -0400

    finished part 2

--- fso_fundamentals/class_project exercise/hangman_project/lib/hangman.rb
+++ fso_fundamentals/class_project exercise/hangman_project/lib/hangman.rb
@@ -45,7 +45,50 @@ class Hangman
    arr.each do |el|
     @guess_word[el] = char
    end
+  end
 
+  def try_guess(char)
+    if self.already_attempted?(char)
+      puts "that has already been attempted"
+      return false
+    end
+    @attempted_chars << char
+    matches = self.get_matching_indices(char)
+    self.fill_indices(char, matches)
+    @remaining_incorrect_guesses-=1 if matches.empty? 
+    true
+  end
+
+  def ask_user_for_guess
+    print 'Enter a char:'
+    self.try_guess(gets.chomp)
+  end
+
+  def win?
+    if @guess_word.join("") == @secret_word
+      print "WIN"
+      return true
+    else
+      return false
+    end
+  end
+
+  def lose?
+    if @remaining_incorrect_guesses == 0
+      print "LOSE"
+      return true
+    else
+      return false
+    end
+  end
+
+  def game_over?
+    if self.win? || self.lose?
+      print "#{@secret_word}"
+      return true
+    else
+      return false
+    end
   end
 end
 
