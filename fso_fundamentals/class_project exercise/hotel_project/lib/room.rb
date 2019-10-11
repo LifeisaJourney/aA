b59a37e9bc4a87cdae103710d735b864facd7473
LifeH <hwie.zhu@gmail.com>
2019-10-11 15:24:02 -0400

    finished part 1

--- fso_fundamentals/class_project exercise/hotel_project/lib/room.rb
+++ fso_fundamentals/class_project exercise/hotel_project/lib/room.rb
@@ -1,3 +1,37 @@
 class Room
+  def initialize(num)
+    @capacity = num 
+    @occupants = []
+  end
 
+  def capacity
+    @capacity
+  end
+
+  def occupants
+    @occupants
+  end
+
+  def full?
+    if @occupants.length < @capacity
+      return false
+    else
+      return true
+    end
+  end
+
+  def available_space
+    return @capacity - @occupants.length
+  end
+
+  def add_occupant(string)
+    
+
+    if !self.full?
+      @occupants << string
+      return true
+    else 
+      return false
+    end
+  end
 end
