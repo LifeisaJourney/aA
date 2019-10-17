28b367bd6ef07f1228577b1aa0873699cbef8aeb
LifeH <hwie.zhu@gmail.com>
2019-10-17 15:24:08 -0400

    finished part 1

--- fso_fundamentals/oop_projects/mastermind_project/lib/code.rb
+++ fso_fundamentals/oop_projects/mastermind_project/lib/code.rb
@@ -6,4 +6,39 @@ class Code
     "Y" => :yellow
   }
 
+  attr_reader :pegs
+
+  def self.valid_pegs?(arr)
+    if arr.all? {|char| POSSIBLE_PEGS.has_key?(char.upcase)}
+      return true
+    else
+      return false
+    end
+  end
+  
+  def self.random(length)
+    random_pegs = []
+    length.times {random_pegs << POSSIBLE_PEGS.keys.sample}
+    Code.new(random_pegs)
+  end
+
+  def self.from_string(pegs)
+    Code.new(pegs.split(""))
+  end
+  
+  def initialize(arr)
+    if Code.valid_pegs?(arr)
+      @pegs = arr.map(&:upcase)
+    else
+      raise "Does not contain valid pegs"
+    end
+  end
+
+  def [](index)
+    return @pegs[index]
+  end
+  
+  def length
+    return @pegs.length
+  end
 end
