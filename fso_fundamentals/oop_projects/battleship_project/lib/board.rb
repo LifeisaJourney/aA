94d6b67cf1d1375da778433d5a70fc381c4cdb20
LifeH <hwie.zhu@gmail.com>
2019-10-18 12:13:45 -0400

    finished part 1

--- fso_fundamentals/oop_projects/battleship_project/lib/board.rb
+++ fso_fundamentals/oop_projects/battleship_project/lib/board.rb
@@ -1,3 +1,21 @@
 class Board
-  
+  attr_reader :size
+  def initialize(n)
+    @grid = Array.new(n) {Array.new(n, :N)}
+    @size = (n * n)
+  end
+
+  def [](position)
+    row, col = position
+    return @grid[row][col]
+  end
+
+  def []=(position, value)
+    row, col = position
+    @grid[row][col] = value
+  end
+
+  def num_ships
+    @grid.flatten.count {|ele| ele == :S}
+  end
 end
