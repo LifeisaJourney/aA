69e38beb206a6bbd2a2cc7d2f1a81a6ca160588b
LifeH <hwie.zhu@gmail.com>
2019-10-19 17:00:28 -0400

    fixed logic in player mode

--- fso_fundamentals/oop_projects/battleship_project/lib/player.rb
+++ fso_fundamentals/oop_projects/battleship_project/lib/player.rb
@@ -1,6 +1,6 @@
 class Player
   def get_move
-    puts "enter a position with coordinates separated with a space like /4 /7"
+    puts "enter a position with coordinates separated with a space like '4 7'"
     response = gets.chomp.split(" ").map(&:to_i)
   end
 
