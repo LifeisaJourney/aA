2ef25fbeb9e54fd476306bbf2cba24dac0558659
LifeH <hwie.zhu@gmail.com>
2019-11-26 10:58:28 -0500

    initial project commit

---
+++ fso_fundamentals/class_project exercise/ghost_project/ghost.rb
@@ -0,0 +1,13 @@
+class Ghost
+    def initialize(player, fragment, dictionary)
+        @fragment = fragment
+        @player = player
+        @dictionary = dictionary
+    end
+
+    def valid_play?
+    end
+    
+    def play_round
+    end
+end
